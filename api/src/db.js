7a72fd8eb60f41c2dc43ae44861cb330c564dd98
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2024-04-09 00:42:11 -0300

    intentando

--- api/src/db.js
+++ api/src/db.js
@@ -2,21 +2,22 @@ require('dotenv').config()
 const { Sequelize, HasMany } = require('sequelize')
 const fs = require('fs')
 const path = require('path')
-// const { DB_USER, DB_PASSWORD, DB_HOST} =
-//   process.env
+const { DB_USER, DB_PASSWORD, DB_HOST} =
+  process.env
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// )
+const sequelize = new Sequelize(
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`,
+  "postgres://default:G0spMuDezCi1@ep-white-dream-27573542.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+)
 
 const basename = path.basename(__filename)
 
