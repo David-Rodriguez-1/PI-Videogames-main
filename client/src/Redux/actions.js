592eff6d7674343ffde959673001a8490b0e9a07
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2024-04-08 23:08:55 -0300

    intento

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -11,8 +11,8 @@ import {
   RESET_FILTER,
 } from "./action_types";
 
-const URL_GAMES = "http://localhost:3001/videogames";
-const URL_GENRES = 'http://localhost:3001/genres'
+const URL_GAMES = "http://localhost:8001/videogames";
+const URL_GENRES = 'http://localhost:8001/genres'
 // const URL_GAMES = 'https://pi-videogames-main-back.vercel.app/'
 // const URL_GENRES = 'https://pi-videogames-main-back.vercel.app/genre'
 
